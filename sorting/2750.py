561f2f25f595ebe561d3e2b339d54a88820d61d5
haebeen <likesun2000@gmail.com>
2023-03-19 00:34:19 +0900

    2750_수 정렬하기

--- sorting/2750.py
+++ sorting/2750.py
@@ -1,9 +1,15 @@
-n = input('')
-list = []
-for i in range(0, int(n)):
-    num = input('')
-    list.append(int(num))
-list.sort()
+n = int(input())
 
-for num in list:
-    print(num)
+# 입력받은 수들을 정수로 변환
+inputList = list(map(int, input().split()))
+
+# set으로 변환하여 중복 제거 후 다시 list로 변환
+inputList = list(set(inputList))
+
+# 정렬
+inputList.sort()
+
+# list의 값을 하나씩 출력
+for num in inputList:
+    # 줄 바꿈 문자를 제거하기 위해 end 매개변수 사용
+    print(num, end=" ")
